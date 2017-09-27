195559d2ae85e6bee7711f45e1c3d3667f4b2488
Emily Woods <emilymonica.woods@gmail.com>
2017-09-27 22:16:21 +0100

    Add test for CSVFileInput

---
+++ src/test/java/io/crate/plugin/csv/CSVFileInputTest.java
@@ -0,0 +1,37 @@
+package io.crate.plugin.csv;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class CSVFileInputTest {
+
+    CSVFileInput subjectUnderTest;
+
+    URI testURI = URI.create("file:///data/example.csv");
+    Predicate<URI> testPredicateURI = Predicate.isEqual(true);
+
+
+    @Before
+    public void setup() {
+        subjectUnderTest = new CSVFileInput();
+    }
+
+    @Test
+    public void listUris_givenURI_thenReturnsListContainingURI() throws IOException {
+        List<URI> result = subjectUnderTest.listUris(testURI, testPredicateURI);
+
+        assertThat(result, is(Collections.singletonList(testURI)));
+    }
+
+}
